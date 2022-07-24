047e4b2e875b22f07fa5b4469bbd3550c140adbd
GreedNeSS <ruslan199126@gmail.com>
2022-07-24 13:13:11 +0300

    Добавьте файлы проекта.

---
+++ 1-Introduction/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _1_Introduction.Filters;
+using System;
+
+namespace _1_Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        [SimpleResourceFilter]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [FakeNotFoundResourceFilter]
+        public IActionResult Private()
+        {
+            return Content("Private Page");
+        }
+    }
+}
---
+++ 1-Introduction/Filters/SimpleResourceFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace _1_Introduction.Filters
+{
+    public class SimpleResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+        }
+    }
+}

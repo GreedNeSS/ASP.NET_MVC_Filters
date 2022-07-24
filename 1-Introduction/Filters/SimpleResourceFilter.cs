29f66bc0221573093f27000e7648647907430cfd
GreedNeSS <ruslan199126@gmail.com>
2022-07-24 13:21:47 +0300

    Added introduction filter example.

--- 1-Introduction/Filters/SimpleResourceFilter.cs
+++ 1-Introduction/Filters/SimpleResourceFilter.cs
@@ -6,11 +6,11 @@ namespace _1_Introduction.Filters
     {
         public void OnResourceExecuted(ResourceExecutedContext context)
         {
-            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
         }
 
         public void OnResourceExecuting(ResourceExecutingContext context)
         {
+            context.HttpContext.Response.Cookies.Append("LastVisit", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
         }
     }
 }
--- 1-Introduction/Program.cs
+++ 1-Introduction/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();

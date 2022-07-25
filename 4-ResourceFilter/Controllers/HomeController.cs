97aff905ac92363aee04e5541e12d2d9911445fa
GreedNeSS <ruslan199126@gmail.com>
2022-07-25 19:29:27 +0300

    Added ResourceFilter example.

---
+++ 4-ResourceFilter/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _4_ResourceFilter.Filters;
+
+namespace _4_ResourceFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [IEAsyncFilter]
+        public IActionResult Index()
+        {
+            return Content("Main Page");
+        }
+    }
+}
---
+++ 4-ResourceFilter/Filters/IEAsyncFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.RegularExpressions;
+
+namespace _4_ResourceFilter.Filters
+{
+    public class IEAsyncFilterAttribute : Attribute, IAsyncResourceFilter
+    {
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            string userAgent = context.HttpContext.Request.Headers["User-Agent"].ToString();
+
+            if (Regex.IsMatch(userAgent, "MSIE|Trident"))
+            {
+                context.Result = new ContentResult { Content = "Ваш браузер устарел!" };
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}

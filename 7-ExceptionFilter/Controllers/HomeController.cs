3a83e9e2a39d840f5c23953915dc61db09be40ad
GreedNeSS <ruslan199126@gmail.com>
2022-07-26 11:28:23 +0300

    Added ExceptionFilter example.

---
+++ 7-ExceptionFilter/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _7_ExceptionFilter.Filters;
+
+namespace _7_ExceptionFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [CustomExceptionFilter]
+        public IActionResult Index()
+        {
+            int x = 0;
+            int y = 8 / x;
+            return Content($"Result: {y}");
+        }
+    }
+}
---
+++ 7-ExceptionFilter/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace _7_ExceptionFilter.Filters
+{
+    public class CustomExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var actionName = context.ActionDescriptor.DisplayName;
+            var exceptionStack = context.Exception.StackTrace;
+            var exceptionMessage = context.Exception.Message;
+            context.Result = new ContentResult
+            {
+                Content = $"В методе {actionName} возникло исключение: {exceptionMessage}. {exceptionStack}"
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
